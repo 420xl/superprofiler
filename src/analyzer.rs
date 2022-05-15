c05b1db48d667cc3d0ff9940152388931bff541c
R. Miles McCain <github@sendmiles.email>
2022-05-15 00:05:18 +0000

    Remember execution state locations

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,30 +1,40 @@
 use crate::coordinator::ExecutionState;
 use crate::instruction::Instruction;
 use log::{debug, info};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::sync::mpsc;
 use crate::utils;
 
 pub struct CodeAnalyzer {
     known_branching_instructions: HashSet<Instruction>,
+    known_branching_addresses: HashSet<u64>,
+    seen_addresses: HashMap<u64, u64>
 }
 
 impl CodeAnalyzer {
     pub fn new() -> Self {
         Self {
             known_branching_instructions: HashSet::new(),
+            known_branching_addresses: HashSet::new(),
+            seen_addresses: HashMap::new()
         }
+    }
+
+    pub fn ingest_execution_state(&mut self, state: &ExecutionState) {
+        let counter = self.seen_addresses.entry(state.address).or_insert(0);
+        *counter += 1;
     }
 
     pub fn ingest_single_step_sequence(&mut self, sequence: Vec<ExecutionState>) {
         for (preceding, following) in sequence.iter().zip(sequence.iter().skip(1)) {
             // First, check if it's a branch
-            if preceding.address + preceding.instruction.instruction_size() as u64
-                != following.address
+            // if preceding.address + preceding.instruction.instruction_size() as u64
+            //     != following.address
+            if utils::offset(preceding.address, following.address) < 0
             {
                 // It's a branch! Add it to the known branching instructions.
-                self.known_branching_instructions
-                    .insert(preceding.instruction);
+                self.known_branching_instructions.insert(preceding.instruction);
+                self.known_branching_addresses.insert(preceding.address);
                 info!(
                     "Detected branch instruction at {}: {:?} (addr offset: {})",
                     preceding.address,
@@ -47,6 +57,7 @@ pub fn analyze(rx: mpsc::Receiver<ExecutionState>) {
     loop {
         match rx.recv() {
             Ok(state) => {
+                analyzer.ingest_execution_state(&state);
                 state_buffer.push(state);
 
                 if state_buffer.len() > 200 {
